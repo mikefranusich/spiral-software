302a8052d3255c285a49cf807566d8658cfebb43
Mike Franusich <mike.franusich@gmail.com>
2025-02-03 10:51:23 -0500

    add skeletal cfunc.c that will support loading dynamic libraries and calling C functions from GAP

---
+++ gap/src/cfunc.c
@@ -0,0 +1,16 @@
+
+//  Copyright (c) 2025, Carnegie Mellon University
+//  See LICENSE for details
+
+//
+// cfunc.c -- support for loading dynamic libraries and calling C functions from GAP
+//
+
+#include <stdio.h>
+
+
+void Init_CFunc() {
+    
+    printf("\n*** Init_CFunc() ***\n");
+    
+}
