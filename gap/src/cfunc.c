9e093ab77257ebc54237576f07206ad9fc6808c5
Mike Franusich <mike.franusich@gmail.com>
2025-02-03 13:29:59 -0500

    add dlfcn.c/.h from github.com/dlfcn-win32, rename to win_dlfcn.c/.h

--- gap/src/cfunc.c
+++ gap/src/cfunc.c
@@ -6,11 +6,47 @@
 // cfunc.c -- support for loading dynamic libraries and calling C functions from GAP
 //
 
+#include <stdlib.h>
 #include <stdio.h>
+
+#ifdef WIN32
+    #include "win_dlfcn.h"
+#else
+    #include <dlfcn.h>
+#endif
+
+
+// for GAP to C functions
+#include "system.h"
+#include "memmgr.h" 
+#include "integer.h"
+#include "args.h"
+#include "eval.h"
+
+
+Obj FunLoadLibrary(Obj hdCall) {
+    char * usage = "usage: LoadLibrary(<library>)";
+    Obj hd;
+    char* libname;
+    
+    if (GET_SIZE_BAG(hdCall) != 2 * SIZE_HD) {
+        return Error(usage, 0, 0);
+    }
+    hd = EVAL( PTR_BAG(hdCall)[1] );
+    libname = (char*)HdToString(hd,
+            "<library> must be a String.\nUsage: %s", (Int)usage, 0);
+            
+    printf("\n*** FunLoadLibrary(%s) ***\n", libname);
+    
+    return INT_TO_HD(123);
+}
+
+
 
 
 void Init_CFunc() {
     
     printf("\n*** Init_CFunc() ***\n");
     
+    InstIntFunc("LoadLibrary", FunLoadLibrary); 
 }
